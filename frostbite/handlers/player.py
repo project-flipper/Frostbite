981606b61f8867da51e1e010a40a7bd569da79ca
DanLCD <44991848+DanLCD@users.noreply.github.com>
2024-10-29 20:24:37 -0600

    Update state on action update

--- frostbite/handlers/player.py
+++ frostbite/handlers/player.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 from fastapi import Depends
-from frostbite.core.socket import send_packet
+from frostbite.core.socket import send_packet, sio
 from frostbite.handlers import get_user_id, packet_handlers
 from frostbite.handlers.room import get_current_room
 from frostbite.models.action import Action
@@ -9,11 +9,17 @@ from frostbite.models.packet import Packet
 
 @packet_handlers.register("player:action")
 async def handle_player_action(
+    sid: str,
     packet: Packet[Action],
     user_id: Annotated[int, Depends(get_user_id)],
     room_key: Annotated[str, Depends(get_current_room)],
     namespace: str,
 ):
+    async with sio.session(sid) as session:
+        session["x"] = packet.d.x
+        session["y"] = packet.d.y
+        session["action"] = packet.d.frame
+
     await send_packet(
         room_key,
         "player:action",
